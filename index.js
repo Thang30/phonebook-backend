be173a8263505e8b1bb4bc16bd5d8185428a8347
thang.ngo <thang.ngo@accenture.com>
2024-06-25 17:34:50 +0300

    uibuild

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 
 const app = express();
-const port = 3001;
+const PORT = process.env.PORT || 3001
 
 const phonebook = [
   { id: '1', name: 'Arto Hellas', number: '040-123456' },
